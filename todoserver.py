51dc3c0475122aa67fcab3eac6cd65e78c7e4997
origCRE <cre@reagan.com>
2017-02-24 18:25:52 -0500

    Cycle of creating a task and fetching its details

--- todoserver.py
+++ todoserver.py
@@ -1,12 +1,31 @@
 # todoserver.py
 
-from flask import Flask, make_response
-
+from flask import Flask, make_response, request
+import json
 app = Flask(__name__)
 
-@app.route("/tasks/")
+MEMORY = {}
+
+@app.route("/tasks/", methods=["GET"])
 def get_all_tasks():
     return make_response("[]", 200)
+
+@app.route("/tasks/", methods=["POST"])
+def create_task():
+    payload = request.get_json(force=True)
+    task_id = 1
+    MEMORY[task_id] = {
+        "summary": payload["summary"],
+        "description": payload["description"],
+        }
+    task_info = {"id": task_id}
+    return make_response(json.dumps(task_info), 201)
+
+@app.route("/tasks/<int:task_id>/")
+def task_details(task_id):
+    task_info = MEMORY[task_id].copy()
+    task_info["id"] = task_id
+    return json.dumps(task_info)
 
 if __name__ == "__main__":
     app.run()
